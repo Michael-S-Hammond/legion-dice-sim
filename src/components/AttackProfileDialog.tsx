aa0d5d4eb2f308f70830dbf5653a787bad23394e
Michael Hammond <michael@torql.com>
2021-06-04 16:56:09 -0500

    Start layout of attack profile dialog

--- src/components/AttackProfileDialog.tsx
+++ src/components/AttackProfileDialog.tsx
@@ -21,7 +21,47 @@ class AttackProfileDialog extends React.Component<AttackProfileDialogProps> {
                             </button>
                         </div>
                         <div className="modal-body">
-                            ...
+                            <div className="btn-group btn-group-toggle" data-toggle="buttons">
+                                <label className="btn btn-light active">
+                                    <input type="radio" name="faction" id="rebel" autoComplete="off" checked/>Rebel
+                                </label>
+                                <label className="btn btn-light ">
+                                    <input type="radio" name="faction" id="empire" autoComplete="off"/>Empire
+                                </label>
+                                <label className="btn btn-light ">
+                                    <input type="radio" name="faction" id="republic" autoComplete="off"/>Republic
+                                </label>
+                                <label className="btn btn-light ">
+                                    <input type="radio" name="faction" id="separatist" autoComplete="off"/>Separatist
+                                </label>
+                            </div>
+                            <div className="btn-group btn-group-toggle" data-toggle="buttons">
+                                <label className="btn btn-light active">
+                                    <input type="radio" name="rank" id="commander" autoComplete="off" checked/>Commander
+                                </label>
+                                <label className="btn btn-light ">
+                                    <input type="radio" name="rank" id="operative" autoComplete="off"/>Operative
+                                </label>
+                                <label className="btn btn-light ">
+                                    <input type="radio" name="rank" id="corps" autoComplete="off"/>Corps
+                                </label>
+                                <label className="btn btn-light ">
+                                    <input type="radio" name="rank" id="specialForces" autoComplete="off"/>Special Forces
+                                </label>
+                                <label className="btn btn-light ">
+                                    <input type="radio" name="rank" id="support" autoComplete="off"/>Support
+                                </label>
+                                <label className="btn btn-light ">
+                                    <input type="radio" name="rank" id="heavy" autoComplete="off"/>Heavy
+                                </label>
+                            </div>
+                            <div>
+                                <select id="unitSelect">
+                                    <option value="0" selected>Luke Skywalker</option>
+                                    <option value="1">Leia Organa</option>
+                                    <option value="2">Han Solo</option>
+                                </select>
+                            </div>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
