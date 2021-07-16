4a8a7aae90bd6b9411d2bdd43efd873eead99c2b
Michael Hammond <michael@torql.com>
2021-07-16 16:00:29 -0500

    More unit tests for AttackInputsApplicator

--- src/test/code/profiles/AttackInputsApplicator.test.ts
+++ src/test/code/profiles/AttackInputsApplicator.test.ts
@@ -109,4 +109,92 @@ describe('AttackInputApplicator', () => {
         const input = AIA.createAttackInputsFromProfile(profile, [], upgrades, defaultTokens);
         testOffenseAndCombatSnapshots(input, 'Stormtroopers - Fragmentation Grenades');
     });
+
+    it('handles armament upgrade', () => {
+        const profile = UP.getUnits().filter(u => u.name === 'Jyn Erso')[0];
+        const upgrades = UC.getUpgrades().filter(u => u.name === 'A-180 Pistol Config');
+
+        const input = AIA.createAttackInputsFromProfile(profile, [], upgrades, defaultTokens);
+        testOffenseAndCombatSnapshots(input, 'Jyn Erso - A-180 Pistol Config');
+    });
+
+    it('handles armament upgrade with base weapon - no arsenal', () => {
+        const profile = UP.getUnits().filter(u => u.name === 'Jyn Erso')[0];
+        const upgrades = UC.getUpgrades().filter(u => u.name === 'A-180 Pistol Config');
+
+        const input = AIA.createAttackInputsFromProfile(profile, [profile.weapons[1]], upgrades, defaultTokens);
+        testOffenseAndCombatSnapshots(input, 'Jyn Erso - A-180 Pistol Config + base weapon');
+    });
+
+    it('handles armament upgrade with base weapon with arsenal', () => {
+        const profile = UP.getUnits().filter(u => u.name === 'General Grievous')[0];
+        const upgrades = UC.getUpgrades().filter(u => u.name === 'DT-57 "Annihialator"');
+
+        const input = AIA.createAttackInputsFromProfile(profile, profile.weapons, upgrades, defaultTokens);
+        testOffenseAndCombatSnapshots(input, 'General Grievous - DT-57 "Annihialator" + base weapon');
+    });
+
+    it('handles counterpart upgrade', () => {
+        const profile = UP.getUnits().filter(u => u.name === 'Iden Versio')[0];
+        const upgrades = UC.getUpgrades().filter(u => u.name === "Iden's ID10 Seeker Droid");
+
+        const input = AIA.createAttackInputsFromProfile(profile, [profile.weapons[1]], upgrades, defaultTokens);
+        testOffenseAndCombatSnapshots(input, "Iden Versio - Iden's ID10 Seeker Droid");
+    });
+
+    it('handles counterpart upgrade', () => {
+        const profile = UP.getUnits().filter(u => u.name === 'BARC Speeder')[0];
+        const upgrades = UC.getUpgrades().filter(u => u.name === "BARC Ion Gunner");
+
+        const input = AIA.createAttackInputsFromProfile(profile, [profile.weapons[1]], upgrades, defaultTokens);
+        testOffenseAndCombatSnapshots(input, "BARC Speeder - BARC Ion Gunner");
+    });
+
+    it('handles force upgrade', () => {
+        const profile = UP.getUnits().filter(u => u.name === 'Darth Vader' && u.subtitle === 'Dark Lord of the Sith')[0];
+        const upgrades = UC.getUpgrades().filter(u => u.name === "Saber Throw");
+
+        const input = AIA.createAttackInputsFromProfile(profile, [profile.weapons[0]], upgrades, defaultTokens);
+        testOffenseAndCombatSnapshots(input, "Darth Vader (Commander) - Saber Throw");
+    });
+
+    it('handles gear upgrade', () => {
+        const profile = UP.getUnits().filter(u => u.name === 'Stormtroopers')[0];
+        const upgrades = UC.getUpgrades().filter(u => u.name === "Targeting Scopes");
+
+        const input = AIA.createAttackInputsFromProfile(profile, [profile.weapons[1]], upgrades, defaultTokens);
+        testOffenseAndCombatSnapshots(input, "Stormtroopers - Targeting Scopes");
+    });
+
+    it('handles generator upgrade', () => {
+        const profile = UP.getUnits().filter(u => u.name === 'E-Web Heavy Blaster Team')[0];
+        const upgrades = UC.getUpgrades().filter(u => u.name === 'Overcharged Generator');
+
+        const input = AIA.createAttackInputsFromProfile(profile, [profile.weapons[2]], upgrades, defaultTokens);
+        testOffenseAndCombatSnapshots(input, 'E-Web - Overcharged Generator');
+    });
+
+    it('handles generator upgrade without fixed position weapon', () => {
+        const profile = UP.getUnits().filter(u => u.name === 'E-Web Heavy Blaster Team')[0];
+        const upgrades = UC.getUpgrades().filter(u => u.name === 'Overcharged Generator');
+
+        const input = AIA.createAttackInputsFromProfile(profile, [profile.weapons[1]], upgrades, defaultTokens);
+        testOffenseAndCombatSnapshots(input, 'E-Web - Overcharged Generator - no fixed position');
+    });
+
+    it('handles hardpoint upgrade', () => {
+        const profile = UP.getUnits().filter(u => u.name === 'TX-130 Saber-class Fighter Tank')[0];
+        const upgrades = UC.getUpgrades().filter(u => u.name === 'TX-130 Twin Laser Turret');
+
+        const input = AIA.createAttackInputsFromProfile(profile, [profile.weapons[0]], upgrades, defaultTokens);
+        testOffenseAndCombatSnapshots(input, 'TX-130 Saber-class Fighter Tank - TX-130 Twin Laser Turret');
+    });
+
+    it('handles hardpoint upgrade - incompatible arc', () => {
+        const profile = UP.getUnits().filter(u => u.name === 'T-47 Airspeeder')[0];
+        const upgrades = UC.getUpgrades().filter(u => u.name === 'Mo/Dk Power Harpoon');
+
+        const input = AIA.createAttackInputsFromProfile(profile, [profile.weapons[0]], upgrades, defaultTokens);
+        testOffenseAndCombatSnapshots(input, 'T-47 Airspeeder - Mo/Dk Power Harpoon');
+    });
 });
